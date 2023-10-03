4f80d2f8947b708cbf3debcd0731b749009d1f7e
mkj <mik.jed2003@gmail.com>
2023-10-03 16:15:57 +0200

    emal verification update

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { signin, signout, signup } from '../controllers/auth.js';
-import { verify } from 'jsonwebtoken';
+import { signin, signout, signup, verify } from '../controllers/auth.js';
 
 const router = express.Router();
 
