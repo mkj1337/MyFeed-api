7b01fd72d861f46689e4d0a8ee5a5a72490130cf
mkj <mik.jed2003@gmail.com>
2023-12-21 14:45:00 +0100

    Update error message for user signup when email already exists.

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,7 +23,7 @@ export const signup = (req, res) => {
     const q = "SELECT * FROM users WHERE email = ?;";
     db.query(q, [req.body.email], (err, data) => {
         if (err) return res.status(500).json(err);
-        if (data.length) return res.status(409).json('User with that email already exists!');
+        if (data.length) return res.status(409).json({ message: 'User with that email already exists!' });
 
         const q = "SELECT * FROM users WHERE username = ?;";
 
