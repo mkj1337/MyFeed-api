d9914a1d6efaa96e3c97221d023227413b4d93f1
MKJ <mik.jed2003@gmail.com>
2024-01-14 22:16:58 +0100

    Delete post media from Cloudinary if it exists.

--- controllers/posts.js
+++ controllers/posts.js
@@ -260,22 +260,25 @@ export const deletePost = (req, res) => {
         const { post_media } = req.body;
         const { postId } = req.params;
 
-        try {
-            if (post_media.length) {
-                for (let i = 0; i < post_media.length; i++) {
-                    if (post_media[i].post_img !== null) {
-                        await cloudinary.uploader.destroy(post_media[i].post_img);
-                    }
+        const deleteMediaFromCloud = async () => {
+            try {
+                if (post_media.length) {
+                    for (let i = 0; i < post_media.length; i++) {
+                        if (post_media[i].post_img !== null) {
+                            await cloudinary.uploader.destroy(post_media[i].post_img);
+                        }
 
-                    if (post_media[i].post_video !== null) {
-                        await cloudinary.uploader.destroy(post_media[i].post_video);
+                        if (post_media[i].post_video !== null) {
+                            await cloudinary.uploader.destroy(post_media[i].post_video);
+                        }
                     }
                 }
+            } catch (err) {
+                console.log(err)
             }
-        } catch (err) {
-            console.log(err)
-        }
+        };
 
+        deleteMediaFromCloud();
 
         const q = "DELETE FROM posts WHERE id=?;";
 
